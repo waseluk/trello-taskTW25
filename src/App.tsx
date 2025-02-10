106d8bf2ad201504a4e8f27cf14370f57507e9a8
waseluk <103688464+waseluk@users.noreply.github.com>
2025-02-10 19:06:51 +0000

    trello-start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Trello Task TW'25</h1>
+      <div className="card">
+        <p>Board here...</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
